a06a890a1a78a5b82d7477e35229c138c78f96a1
Vadim Sobolev <vadsobolev@yandex.ru>
2023-05-28 00:51:12 +0300

    dz1

---
+++ dz_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает
+// вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());    
+int Length = number.ToString().Length;                         //Преобразуем из одного формата в другой
+
+if (Length == 3)
+{
+    int result = (number/10) % 10;
+    Console.WriteLine("Вторая цифра числа:" + result);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трёхзначное число");    
+}
